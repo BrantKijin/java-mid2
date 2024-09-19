83066d67dd4a79647d33cad3fd8cdb8f64ca5f37
곽기진 <upper0house@gmail.com>
2024-09-19 17:51:48 +0900

    feat: init

---
+++ src/main/java/com/generic/ex1/BoxMain1.java
@@ -0,0 +1,18 @@
+package com.generic.ex1;
+
+import java.sql.SQLOutput;
+
+public class BoxMain1 {
+	public static void main(String[] args) {
+		IntegerBox integerBox = new IntegerBox();
+		integerBox.set(10);
+		Integer integer = integerBox.get();
+		System.out.println("integer = " + integer);
+
+		StringBox stringBox = new StringBox();
+		stringBox.set("hello");
+		String str = stringBox.get();
+		System.out.println("str = " + str);
+
+	}
+}
